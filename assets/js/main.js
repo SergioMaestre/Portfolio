47e9ec6ceddab216d9c7f58bf27f9defcb5935f1
SergioMaestre <163492980+SergioMaestre@users.noreply.github.com>
2024-04-12 12:32:48 -0500

    Actualizando

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// import parallax from './components/parallax.js';
+// import activeMenu from './components/selected_menu.js';
+// import updateCopyright from './helpers/date_updater.js';
+import resetToHome from './helpers/reload_page.js';
+import sendEmail from './helpers/send_form.js';
+// import changeLanguage from './helpers/change_language.js';
+// import setThemeOnLoad from './helpers/dark_mode.js';
+
+// parallax();
+
+// activeMenu();
+
+resetToHome();
+
+// updateCopyright();
+
+sendEmail();
+
+// // changeLanguage();
+
+// setThemeOnLoad();
